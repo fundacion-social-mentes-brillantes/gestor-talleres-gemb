495f1d35ace930df547fe39f557aba42fac54814
fundacionsocial-debug <fundacionsocial@gimnasioemocionalmb.com>
2026-05-08 15:58:54 -0500

    feat: route production entry to premium app

--- Organizador de tareas/src/main.tsx
+++ Organizador de tareas/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppSafe.tsx';
+import App from './AppPro.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
