6479ff97c4d45b56a86cd5b385db707b433ad8ee
fundacionsocial-debug <fundacionsocial@gimnasioemocionalmb.com>
2026-05-08 17:52:01 -0500

    fix(import): enable CSV import from dashboard button

--- Organizador de tareas/src/main.tsx
+++ Organizador de tareas/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppSecure.tsx';
+import App from './AppCsvImportPatch.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
